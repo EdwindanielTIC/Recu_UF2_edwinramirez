202a12e73003078dd25ec531c1aa6928230103a8
danielramirez <2024_edwin.ramirez@iticbcn.cat>
2025-06-02 16:40:08 +0200

    Exercici 3 mostrar info client

---
+++ serveis/users.py
@@ -0,0 +1,22 @@
+from config.database import connection_db
+
+def inserirusuarios(nombre,cogno,correo,descripcion,curso,any,direccio,codgiP,password):
+    conn = connection_db()
+    cur = conn.cursor()
+    sql = "INSERT INTO users (nombre,cognom,correo,descripcion,curso,any,direccio,codgiP,password) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    values = (nombre,cogno,correo,descripcion,curso,any,direccio,codgiP,password)
+    cur.execute(sql,(values))
+    conn.commit()
+    cur.close()
+    return {"msg": "Usuario incertado correctamente"}
+
+
+def selecionarUsuario(nombre,cognom,correo,curso,any,direccio):
+    conn = connection_db()
+    cur = conn.cursor()
+    sql = "SELECT nombre,cognom,correo,curso,any,direccio FROM users"
+    cur.execute(sql,nombre,cognom,correo,curso,any,direccio)
+    devolverUsers = cur.fetchall()
+    conn.close()
+    cur.close()
+    return devolverUsers
